4e2cbe1173cbc1a4271b307682afdd4fecff81b0
Finn Galanos <finn@webmasterfreelanceonlinedev.site>
2019-09-04 09:51:48 +0400

    Defined the application main structure, created core,shared and admin modules

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,21 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {throwIfAlreadyLoaded} from './guards/module-imports.guard';
+
+@NgModule({
+    declarations: [HeaderComponent, FooterComponent],
+    imports: [
+        CommonModule
+    ],
+    exports: [
+        HeaderComponent,
+        FooterComponent
+    ]
+})
+export class CoreModule {
+    constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+        throwIfAlreadyLoaded(parentModule, 'CoreModule');
+    }
+}

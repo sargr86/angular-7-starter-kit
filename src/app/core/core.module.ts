e3a6e038c855932bed4f10d1850e43b1d1a94180
Finn Galanos <finn@webmasterfreelanceonlinedev.site>
2019-09-04 10:46:33 +0400

    Added login/register components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,13 +1,19 @@
 import {NgModule, Optional, SkipSelf} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
+import {HeaderComponent} from './components/header/header.component';
+import {FooterComponent} from './components/footer/footer.component';
 import {throwIfAlreadyLoaded} from './guards/module-imports.guard';
+import {LoginComponent} from './components/login/login.component';
+import {RegisterComponent} from './components/register/register.component';
+import {MaterialModule} from './modules/material.module';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
-    declarations: [HeaderComponent, FooterComponent],
+    declarations: [HeaderComponent, FooterComponent, LoginComponent, RegisterComponent],
     imports: [
         CommonModule,
+        MaterialModule,
+        SharedModule
     ],
     exports: [
         HeaderComponent,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from '../core/modules/material.module';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule
+    ],
+    exports:[
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule
+    ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
